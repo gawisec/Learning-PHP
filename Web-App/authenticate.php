f901db5b7f702b485827086ec5979cfe5d4cfb28
agamestory <gawisec@pm.me>
2018-07-29 18:45:58 +0800

    Added several files.

---
+++ Web-App/authenticate.php
@@ -0,0 +1,20 @@
+<?php
+    setcookie('username', $_POST['username']);
+    $submitted = !empty($_POST);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bookstore</title>
+</head>
+<body>
+    <p>Form submitted? <?php echo (int) $submitted; ?></p>
+    <p> Your login info is</p>
+
+    <ul>
+        <li><b>Username</b>: <?php echo $_POST['username']; ?></li>
+        <li><b>Password</b>: <?php echo $_POST['password']; ?></li>
+</body>
+</html>
