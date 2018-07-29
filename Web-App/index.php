96c2ff43bdfc0df3b3709a57bb0508f5aead6bb2
agamestory <gawisec@pm.me>
2018-07-29 18:59:57 +0800

    Some update.

--- Web-App/index.php
+++ Web-App/index.php
@@ -9,12 +9,29 @@ $looking = isset($_GET['title']) || isset($_GET['author']);
     <title>Bookstore</title>
 </head>
 <body>
-    <p>You lookin`? <?php echo (int) $looking; ?> </p>
-    <p> The book you are looking for is </P>
-    <ul>
-        <li><b>Title</b>: <?php echo $_GET['title']; ?></li>
-        <li><b>Author</b>: <?php echo $_GET['author']; ?></li>
-    </ul>
+    <p>
+        <?php
+            if (isset($_COOKIE['username'])) {
+                echo "You are " . $_COOKIE['username'];
+            } else {
+                echo "You are not authenticated.";
+            }
+        ?>
+    </p>
+    <?php
+        if (isset($_GET['title']) && isset($_GET['author'])) {
+    ?>
+        <p>The book you are looking for is</p>
+        <ul>
+            <li><b>Title</b>: <?php echo $_GET['title']; ?></li>
+            <li><b>Author</b>: <?php echo $_GET['author']; ?></li>
+        </ul>
+        <?php
+        } else {
+        ?>
+        <p> You are not looking for a book?</p>    
+    <?php
+    }
+    ?>
 </body>
 </html>
-
